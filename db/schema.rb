2d75da433a684e045c81a62ed931c2fd3d94e953
yusufurur <yusufurur@gmail.com>
2023-06-03 16:07:37 -0400

    User Authentification

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 2021_06_26_062916) do
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
